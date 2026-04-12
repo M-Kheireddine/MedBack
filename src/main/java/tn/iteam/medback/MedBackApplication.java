907e8f0290d754cae038da201b767a47c0c2481a
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2026-04-12 14:03:16 +0100

    chore(US-0001): init MedBack project

---
+++ src/main/java/tn/iteam/medback/MedBackApplication.java
@@ -0,0 +1,13 @@
+package tn.iteam.medback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/iteam/medback/MedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.iteam.medback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
