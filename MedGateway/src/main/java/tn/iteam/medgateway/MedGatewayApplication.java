42645c634188f60eb843d407b6b855fc5063f650
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2026-04-26 23:34:55 +0100

    chore(US-0001): init project: MedGateway

---
+++ MedGateway/src/main/java/tn/iteam/medgateway/MedGatewayApplication.java
@@ -0,0 +1,13 @@
+package tn.iteam.medgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedGatewayApplication.class, args);
+    }
+
+}
