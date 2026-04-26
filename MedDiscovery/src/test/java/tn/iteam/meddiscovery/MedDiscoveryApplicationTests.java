cb31f9f7ae3b9d4a623545554e5a4c8c4bc6bbd1
kheireddine mechergui <kheireddine.dev.ops@gmail.com>
2026-04-26 21:46:34 +0100

    chore(US-0001): init project: MedDiscovery

---
+++ MedDiscovery/src/test/java/tn/iteam/meddiscovery/MedDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.iteam.meddiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
